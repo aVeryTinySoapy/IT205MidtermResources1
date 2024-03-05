83b27d161fc4184644a03d48877d41d4e52475a0
AnitaMaxWynn <157451615+aVeryTinySoapy@users.noreply.github.com>
2024-03-05 15:43:22 +0800

    Add files via upload

---
+++ input.py
@@ -0,0 +1,8 @@
+num1 = input("Enter First Number: ")
+
+num2 = input("Enter Second Number: ")
+
+sum = int(num1) + int(num2)
+
+print("The sum is", sum)
+
---
+++ output.py
@@ -0,0 +1,16 @@
+#Variable Declarations
+x = 25
+messege = "HUUUUUH?"
+ratio = 3.24
+
+#checking the type of data with type() method
+print(type(messege))
+print(type(ratio))
+print(type(x))
+
+#String Formatting
+print(messege + "My name is jessa. I am " + str(x) + " years old")
+print(f"{messege}My Name is Jessa. I am {x} years old")
+
+
+
